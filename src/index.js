657197bf8ab6eaf8930c8796efbb4b71807bb303
dhrumil93 <dhrumilpatel0093@gmail.com>
2024-03-07 11:09:29 +0530

    Connecting DB

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants";
-(async () => {
+import { Express } from "express";
+
+const app = express()(async () => {
   try {
     await mongoose.connect(`${process.env.MONGODB_URI} / ${DB_NAME}`);
+    app.on("Error", (error) => {
+      console.log("Error:", error);
+      throw error;
+    });
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening on PORT ${process.env.PORT}`);
+    });
   } catch (error) {
     console.log("Error:", error);
     throw err;
