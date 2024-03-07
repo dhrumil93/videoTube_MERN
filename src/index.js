742b3203bf11c6b2bef18f516640b86cbf785014
dhrumil93 <dhrumilpatel0093@gmail.com>
2024-03-07 11:00:07 +0530

    Connecting DB

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import dotenv from "dotenv"
-import connectDB from "./db/index.js";
-
-dotenv.config({
-    path: './env'
-})
-
-connectDB()
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI} / ${DB_NAME}`);
+  } catch (error) {
+    console.log("Error:", error);
+    throw err;
+  }
+})();
