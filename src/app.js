a7d39397b027b69eeb05a9fd7104bfa25f42e0a6
dhrumil93 <dhrumilpatel0093@gmail.com>
2024-03-25 19:34:44 +0530

    Cors Use in depth

--- src/app.js
+++ src/app.js
@@ -1,13 +1,19 @@
-import  Express  from "express";
-import cors from "cors"
+import Express from "express";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 
-const app = express()
+const app = express();
 
-app.use( cors({
-    origin: process.env.CORS_ORIGIN ,
-    credentials: true
-}))
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 
+app.use(express.json({ limit: "16KB" }));
+app.use(express.urlenocded({ extended: true, limit: "16KB" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
-export {app}
+export { app };
