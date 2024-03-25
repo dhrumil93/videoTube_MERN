8969dd3f42a8de803ba725f3b2a2a7d9c17e9753
dhrumil93 <dhrumilpatel0093@gmail.com>
2024-03-25 18:25:26 +0530

    Introduction to CORS package and it's use

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
 import  Express  from "express";
+import cors from "cors"
+import cookieParser from "cookie-parser";
 
 const app = express()
+
+app.use( cors({
+    origin: process.env.CORS_ORIGIN ,
+    credentials: true
+}))
 
 
 export {app}
