9a270dc08f57f6d6718122579aae63c8cb50809d
dhrumil93 <dhrumilpatel0093@gmail.com>
2024-03-26 11:25:39 +0530

    Users Data Model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,14 +1,50 @@
 import mongoose, { Schema } from "mongoose";
 
-const userSchema = new Schema({
-  userName: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-    index: true,
+const userSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, // cloud URL
+      required: true,
+    },
+    coverImage: {
+      type: String,
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: string,
+    },
   },
-});
+  { timestamps: true }
+);
 
 export const User = mongoose.model("User", userSchema);
