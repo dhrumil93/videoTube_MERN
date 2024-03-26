b1df3e22163b2e7788b9dee8ae7b040de7ac4a2c
dhrumil93 <dhrumilpatel0093@gmail.com>
2024-03-26 12:29:07 +0530

    User Model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -53,4 +53,8 @@ userSchema.pre("save", async function (next) {
   this.password = bcypt.hash(this.password, 10);
   next();
 });
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password , this.password )
+}
 export const User = mongoose.model("User", userSchema);
