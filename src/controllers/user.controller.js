5e08a828b8a53b3027ee199bd7d3e97ed527bd28
dhrumil93 <dhrumilpatel0093@gmail.com>
2024-03-29 17:06:11 +0530

    few comments

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,4 +5,17 @@ const registerUser = asyncHandler(async (req, res) => {
     message: "OK",
   });
 });
-export {registerUser}
+export { registerUser };
+
+/*
+User details from front end
+validation on backend - not null
+if user is already registered : email , username
+check for images and avatar
+upload on cloudinary
+create user object entry in db
+remove password and refresh token
+user creation
+return response
+
+ */
