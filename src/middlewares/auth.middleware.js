ef28e39e142af4278525c04bc0430b161a937962
dhrumil93 <dhrumilpatel0093@gmail.com>
2024-04-05 12:06:55 +0530

    Authentication Middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -4,13 +4,26 @@ import { Jwt } from "jsonwebtoken";
 import { User } from "../models/user.model.js";
 
 export const verifyJWT = asyncHandler(async (req, res, next) => {
-  const token =
-    req.cookies?.accessToken || req.header("Authorization")?.("Bearer", " "); // to retrieve cookie
+  try {
+    const token =
+      req.cookies?.accessToken || req.header("Authorization")?.("Bearer", " "); // to retrieve cookie
 
-  if (!token) {
-    throw new ApiError(401, "Unauthorized Request !!");
+    if (!token) {
+      throw new ApiError(401, "Unauthorized Request !!");
+    }
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    const user = await User.findById(decodedToken?._id).select(
+      "-password -refreshToken"
+    );
+
+    if (!user) {
+      throw new ApiError(404, "Invalid Access !!");
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid Access!!");
   }
-  const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-
-  await User.findById(decodedToken?._id).select("-password -refreshToken");
 });
