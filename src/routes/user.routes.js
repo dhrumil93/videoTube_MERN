117f5ac128d96607ee218d4a4bc8234e7ec65825
dhrumil93 <dhrumilpatel0093@gmail.com>
2024-04-08 11:04:34 +0530

    refreshAccessToken Route Created

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,6 +3,7 @@ import {
   logOutUser,
   loginUser,
   registerUser,
+  refreshAccessToken
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -25,4 +26,5 @@ router.route("/login").post(loginUser);
 // Secured Route
 
 router.route("/logout").post(verifyJWT, logOutUser);
+router.route("/refresh-token").post(refreshAccessToken);
 export default router;
